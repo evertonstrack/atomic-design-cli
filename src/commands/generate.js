d8876fec059ac2c8a3f506a53da6194c6387fdd1
Everton Strack <everton.strack@cyberweb.com.br>
2019-07-29 16:29:12 -0300

    Extract logic for extension

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -6,20 +6,14 @@ module.exports = {
   description: 'Create new',
   run: async toolbox => {
     const {
-      template,
-      strings,
-      patching,
-      filesystem,
+      createComponent,
       print: { success, error}
     } = toolbox;
 
-    // // Exit process if no name
-    // if (!parameters.first) {
-    //   error('Name must be specified');
-    //   return;
-    // }
 
-    // type of component
+    /**
+     * Question type of component
+     */
     const askType = {
       type: 'select',
       name: 'type',
@@ -34,113 +28,42 @@ module.exports = {
       autofocus: 'atoms'
     }
 
-    const askName = { type: 'input', name: 'name', message: 'What is the name? eg: my-component ' }
+    /**
+     * Question name of component
+     */
+    const askName = { 
+      type: 'input', 
+      name: 'name', 
+      message: 'What is the name? eg: my-component'
+    }
 
-    // ask a series of questions
+    /**
+     * Ask a series of questions
+     */
     const questions = [askType, askName]
     const { type, name } = await prompt(questions);
 
-    console.log('type', type);
-    console.log('name', name);
-
+    /**
+     * Validate if type its selected
+     */
     if (!type) {
       error('Type must be specified.');
       return;
     }
 
+    /**
+     * Validate if name its already filled
+     */
     if (!name) {
       error('Name must be specified.');
       return;
     }
 
-    const folderName = strings.kebabCase(name);
-    const fileName = `${folderName}`;
-    const componentName = strings.pascalCase(name);
-    const folder = `src/components/${type}/${folderName}/`;
-
     /**
-     * Verify if component exists
+     * Create component files
      */
-    const componentExists = filesystem.exists(folder);
-    if( componentExists ) {
-      error(`ERROR: Component already exists.`);
-      return;
-    }
+    await createComponent(type, name);
 
-    /**
-     * Generate component controller
-     */ 
-    await template.generate({
-      template: 'component.ts.ejs',
-      target: `${folder}${fileName}.component.ts`,
-      props: { componentName, fileName },
-    });
-    success(`Generate ${folder}${fileName}.component.ts`);
-
-    /**
-     * Generate component test
-     */
-    await template.generate({
-      template: 'component.spec.ts.ejs',
-      target: `${folder}${fileName}.component.spec.ts`,
-      props: { componentName, fileName },
-    });
-    success(`Generate ${folder}${fileName}.component.spec.ts`);
-
-    /**
-     * Generate component tempalte
-     */
-    await template.generate({
-      template: 'component.html.ejs',
-      target: `${folder}${fileName}.component.html`,
-      props: { name },
-    });
-    success(`Generate ${folder}${fileName}.component.html`);
-    
-    /**
-     * Generate component style
-     */
-    await template.generate({
-      template: 'component.scss',
-      target: `${folder}${fileName}.component.scss`,
-    });
-    success(`Generate ${folder}${fileName}.component.scss`);
-
-    /**
-     * Generate module if its not an 'atom'
-     */
-    if( type !== 'atoms' ) {
-
-      const isPage = type === 'pages';
-
-      await template.generate({
-        template: 'module.ts.ejs',
-        target: `${folder}${fileName}.module.ts`,
-        props: { 
-          componentName, 
-          fileName,
-          isPage
-        },
-      });
-      success(`Generate ${folder}${fileName}.module.ts`);
-
-      // Generate routing file if component its a Page
-      if( isPage ) {
-        await template.generate({
-          template: 'routing.ts.ejs',
-          target: `${folder}${fileName}-routing.module.ts`,
-          props: { componentName },
-        });
-        success(`Generate ${folder}${fileName}-routing.module.ts`);
-      }
-    }
-    
-    // await patching.patch('src/components/atoms/atoms.module.ts', {
-    //   insert: `${name}Component, `, after: 'declarations: ['
-    // });
-    
-    // await patching.patch('src/components/atoms/atoms.module.ts', {
-    //   insert: `\nimport { ${name}Component } from './${fileName}';`, after: `import { CommonModule } from '@angular/common';`,
-    // });
+    success('DONE!');
   }
 }
--- src/extensions/create-component.js
+++ src/extensions/create-component.js
@@ -1,27 +1,114 @@
 module.exports = (toolbox) => {
   const {
-    system, 
+    template,
+    strings,
+    patching,
+    filesystem,
     print: { success, error } 
   } = toolbox;
 
-  async function createComponent(folder, name) {
+  async function createComponent(type, name) {
 
-    // Exit process if no name
-    if (!name) {
-      error('Name must be specified');
+    /**
+     * kebabCase: Format file name for 'componenet-name' format 
+     * pascalCase: Format file name for 'ComponentName' format 
+     */
+    const fileName = strings.kebabCase(name);
+    const componentName = strings.pascalCase(name);
+    const folder = `src/components/${type}/${fileName}/`;
+
+    /**
+     * Verify if component exists
+     */
+    const componentExists = filesystem.exists(folder);
+    if( componentExists ) {
+      error(`ERROR: Component already exists.`);
       return;
     }
 
-    // Create routing with its page
-    const routing = folder.includes('pages') ? '--routing=true' : '';
+    /**
+     * Generate component controller
+     */ 
+    await template.generate({
+      template: 'component.ts.ejs',
+      target: `${folder}${fileName}.component.ts`,
+      props: { componentName, fileName },
+    });
+    success(`Generate ${folder}${fileName}.component.ts`);
 
-    // Execute Angular CLI commands to generate files
-    const generated = await system.run(`ng g m ${folder}/${name} ${routing} && ng g c ${folder}/${name}`);
+    /**
+     * Generate component test
+     */
+    await template.generate({
+      template: 'component.spec.ts.ejs',
+      target: `${folder}${fileName}.component.spec.ts`,
+      props: { componentName, fileName },
+    });
+    success(`Generate ${folder}${fileName}.component.spec.ts`);
 
-    // Generate Angular CLI logs
-    console.log(generated);
+    /**
+     * Generate component tempalte
+     */
+    await template.generate({
+      template: 'component.html.ejs',
+      target: `${folder}${fileName}.component.html`,
+      props: { name },
+    });
+    success(`Generate ${folder}${fileName}.component.html`);
     
-    success(`Generated ${folder}/${name}`);
+    /**
+     * Generate component style
+     */
+    await template.generate({
+      template: 'component.scss',
+      target: `${folder}${fileName}.component.scss`,
+    });
+    success(`Generate ${folder}${fileName}.component.scss`);
+
+    /**
+     * Generate module if its not an 'atom'
+     */
+    if( type !== 'atoms' ) {
+
+      const isPage = type === 'pages';
+
+      await template.generate({
+        template: 'module.ts.ejs',
+        target: `${folder}${fileName}.module.ts`,
+        props: { 
+          componentName, 
+          fileName,
+          isPage
+        },
+      });
+      success(`Generate ${folder}${fileName}.module.ts`);
+
+      // Generate routing file if component its a Page
+      if( isPage ) {
+        await template.generate({
+          template: 'routing.ts.ejs',
+          target: `${folder}${fileName}-routing.module.ts`,
+          props: { componentName },
+        });
+        success(`Generate ${folder}${fileName}-routing.module.ts`);
+      }
+    }
+
+
+    /**
+     * TODO: Check if atoms.module.ts already exists
+     * IF exists: patch file if a new atom component
+     * ELSE: Create a new module;
+     */
+    
+    // await patching.patch('src/components/atoms/atoms.module.ts', {
+    //   insert: `${name}Component, `, after: 'declarations: ['
+    // });
+    
+    // await patching.patch('src/components/atoms/atoms.module.ts', {
+    //   insert: `\nimport { ${name}Component } from './${fileName}';`, after: `import { CommonModule } from '@angular/common';`,
+    // });
+
   }
 
   toolbox.createComponent = createComponent;
